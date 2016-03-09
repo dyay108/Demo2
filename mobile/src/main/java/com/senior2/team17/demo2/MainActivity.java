e20a11e7a969a4135b4b831c078cb572e9350c32
dyay108 <djboi94@gmail.com>
2016-03-09 18:01:24 -0500

    Initial commit

---
+++ mobile/src/main/java/com/senior2/team17/demo2/MainActivity.java
@@ -0,0 +1,38 @@
+package com.senior2.team17.demo2;
+
+import android.app.Notification;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+    }
+    public void sendNotification(View view) {
+        String toSend = editText.getText().toString();
+        if(toSend.isEmpty())
+            toSend = "You sent an empty notification";
+        Notification notification = new NotificationCompat.Builder(getApplication())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Dave and John")
+                .setContentText(toSend)
+                .extend(new NotificationCompat.WearableExtender().setHintShowBackgroundOnly(true))
+                .build();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplication());
+        int notificationId = 1;
+        notificationManager.notify(notificationId, notification);
+    }
+}
